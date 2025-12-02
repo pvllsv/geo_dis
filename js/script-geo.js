32a893c8930f0afc9fe2d640f9e1b93376b46fce
lisov <pawel.lisov2015@yanex.ru>
2025-12-03 00:51:16 +0300

    правки

--- js/script-geo.js
+++ js/script-geo.js
@@ -16,4 +16,19 @@ window.addEventListener('DOMContentLoaded', () =>{
 
         }
     })
+
+    const anchors = document.querySelectorAll('a[href*="#"]');
+
+    for (let anchor of anchors) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const blockID = anchor.getAttribute('href').substr(1);
+
+            document.getElementById(blockID).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    }
 })
